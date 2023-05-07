7e26faaa9a8812105a8387f31529cd927165336d
Guillaume Leclout <124732293+MrG-L@users.noreply.github.com>
2023-05-07 21:42:26 +0200

    post route for movies & users set up

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
 
 const app = express();
+
+app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send('Welcome to my favourite movie list');
 };
 
-app.get("/", welcome);
+app.get('/', welcome);
 
-const movieHandlers = require("./movieHandlers");
-const users = require("./users");
+const movieHandlers = require('./movieHandlers');
+const users = require('./users');
 
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get('/api/movies', movieHandlers.getMovies);
+app.get('/api/movies/:id', movieHandlers.getMovieById);
 
-app.get("/api/users", users.getUsers);
-app.get("/api/users/:id", users.getUserById);
+app.get('/api/users', users.getUsers);
+app.get('/api/users/:id', users.getUserById);
+
+app.post('/api/movies', movieHandlers.postMovie);
+
+app.post('/api/users', users.postUser);
 
 app.listen(port, (err) => {
   if (err) {
-    console.error("Something bad happened");
+    console.error('Something bad happened');
   } else {
     console.log(`Server is listening on ${port}`);
   }
--- movieHandlers.js
+++ movieHandlers.js
@@ -1,4 +1,4 @@
-const database = require("./database");
+const database = require('./database');
 
 // const movies = [
 //   {
@@ -27,17 +27,15 @@ const database = require("./database");
 //   },
 // ];
 
-
-
 const getMovies = (req, res) => {
   database
-    .query("select * from movies")
+    .query('select * from movies')
     .then(([movies]) => {
       res.json(movies);
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send("Error retrieving data from database");
+      res.status(500).send('Error retrieving data from database');
     });
 };
 
@@ -45,21 +43,39 @@ const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
   database
-    .query("select * from movies where id = ?", [id])
+    .query('select * from movies where id = ?', [id])
     .then(([movies]) => {
       if (movies[0] != null) {
         res.json(movies[0]);
       } else {
-        res.status(404).send("Not Found");
+        res.status(404).send('Not Found');
       }
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send("Error retrieving data from database");
+      res.status(500).send('Error retrieving data from database');
+    });
+};
+
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      'INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)',
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error saving the movie');
     });
 };
 
 module.exports = {
   getMovies,
   getMovieById,
+  postMovie,
 };
